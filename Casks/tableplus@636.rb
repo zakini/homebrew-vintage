f7b13136e2bca50a08d957281c052f33136a29cc
Josh Wells <josh@zakini.co.uk>
2026-06-29 17:30:10 +0100

    Added tableplus@636

---
+++ Casks/tableplus@636.rb
@@ -0,0 +1,27 @@
+cask "tableplus@636" do
+  version "6.7.1,636"
+  sha256 "c9777e1eae323fa832a1c099b3f7ec2d78463060175ab36af5dc53a7291d33b3"
+
+  url "https://files.tableplus.com/macos/#{version.csv.second}/TablePlus.dmg"
+  name "TablePlus"
+  desc "Native GUI tool for relational databases"
+  homepage "https://tableplus.com/"
+
+  livecheck do
+    url "https://tableplus.com/osx/version.xml"
+    strategy :sparkle
+  end
+
+  auto_updates true
+  depends_on :macos
+
+  app "TablePlus.app"
+
+  zap trash: [
+    "~/Library/Application Support/com.tinyapp.TablePlus",
+    "~/Library/Caches/com.tinyapp.TablePlus",
+    "~/Library/Cookies/com.tinyapp.TablePlus.binarycookies",
+    "~/Library/Preferences/com.tinyapp.TablePlus.plist",
+    "~/Library/Saved Application State/com.tinyapp.TablePlus.savedState",
+  ]
+end
